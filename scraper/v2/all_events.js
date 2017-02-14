9ef10cd3cb1c198729bc76734354eff6f1655b57
romainbessugesmeusy <romainbessuges@gmail.com>
2017-02-14 21:44:12 +0100

    all

--- scraper/v2/all_events.js
+++ scraper/v2/all_events.js
@@ -1,11 +1,7 @@
-var unique = require('array-unique');
 var async = require('async');
-var writer = require('../writer');
 var options = require('./options');
-var getEvent = require('../lib/getEvent');
 var exec = require('../lib/exec');
 
 async.forEach(options.combinations, function (combination, eachCombination) {
     exec.event({event: combination.id, lang: combination.lang}, eachCombination);
 });
-module.exports = run;
---
+++ scraper/v2/all_scoreboards.js
@@ -0,0 +1,4 @@
+var options = require('./options');
+var exec = require('../lib/exec');
+
+exec.scoreboards(options);
