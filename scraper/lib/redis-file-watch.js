b2d7e5fb666fd3b9cdb61389f6cf3bf955e1b26b
romainbessugesmeusy <romainbessuges@gmail.com>
2017-04-05 14:57:30 +0200

    debug

--- scraper/lib/redis-file-watch.js
+++ scraper/lib/redis-file-watch.js
@@ -1,4 +1,9 @@
 var NRP = require('node-redis-pubsub');
 var config = {host: '10.3.3.3'};
 var nrp = new NRP(config);
-module.exports = nrp.on;
+module.exports = function (channel, handler) {
+    nrp.on(channel, function (filename) {
+        console.info('redis watch', channel, filename);
+        handler(filename, channel);
+    });
+}
